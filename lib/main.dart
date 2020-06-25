6c2cbefcde7ab4e2ca5e78facbe5b7197d228672
Mileba <mykellforbes@gmail.com>
2020-06-25 21:45:33 +0100

    created questions class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrian quizBrian = QuizBrian();
 
 void main() => runApp(Quizzler());
 
@@ -28,11 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  List<Question> questionBank = [
-    Question(q:'You can lead a cow down stairs but not up stairs.',a:false),
-    Question(q:'Approximately one quarter of human bones are in the feet.',a:true),
-    Question(q:'A slug\'s blood is green.',a: true)
-  ];
 
   int questionNumber = 0;
 
@@ -52,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrian.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +75,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                  bool correctAnswer = quizBrian.questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == true) {
                     print('right');
                   } else {
@@ -106,7 +103,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
 
                 setState(() {
-                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                  bool correctAnswer = quizBrian.questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == false) {
                     print('right');
                   } else {
