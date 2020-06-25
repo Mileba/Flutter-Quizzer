697d3459b96ae3437b91fbd9b374e12fae5b5d2d
Mileba <mykellforbes@gmail.com>
2020-06-25 21:02:27 +0100

    created questions class

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,11 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questionBank = [
+    Question(q:'You can lead a cow down stairs but not up stairs.',a:false),
+    Question(q:'Approximately one quarter of human bones are in the feet.',a:true),
+    Question(q:'A slug\'s blood is green.',a: true)
   ];
-
-  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
@@ -54,7 +52,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -80,7 +78,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == true) {
                     print('right');
                   } else {
@@ -108,7 +106,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
 
                 setState(() {
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer = questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == false) {
                     print('right');
                   } else {
