00967c7aeefd9befb3f2b0777566ba8be9115dfb
Mileba <mykellforbes@gmail.com>
2020-06-26 05:14:53 +0100

    test

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,14 +1,10 @@
-# Quizzler ❓
+import 'question.dart';
 
-We’re going to make a quiz app that tests your general knowledge.
-As one of the most popular types of apps on the app stores,
- you can create your own quiz for other people to enjoy!
+class QuizBrain {
+  int _questionNumber = 0;
 
-![Finished App](https://github.com/londonappbrewery/Images/blob/master/quizzler-demo.gif)
-## Code Snippet for Project
-
-```
-Question('Some cats are actually allergic to humans', true),
+  List<Question> _questionBank = [
+    Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
@@ -33,6 +29,25 @@ Question('Some cats are actually allergic to humans', true),
     Question(
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
+  ];
 
-```
-=======
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getCorrectAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
+
+  //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+
+  //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
+}
